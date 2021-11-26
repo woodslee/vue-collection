b4432c39300460c34bc34a080609f508edd4134c
lishulin <lishulin@enovatemotors.com>
2021-11-26 19:19:28 +0800

    create new empty layout

--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
-import Vue from "vue";
-import App from "./App.vue";
+import Vue from 'vue'
+import App from './App'
+import 'normalize.css/normalize.css' // A modern alternative to CSS resets
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import '@/styles/index.scss' // global css
+import '@/icons' // icon
+import store from './store'
+import router from './router'
 
-Vue.config.productionTip = false;
+// set ElementUI lang to EN
+// Vue.use(ElementUI, { locale })
+// 如果想要中文版 element-ui，按如下方式声明
+Vue.use(ElementUI, {
+  size: 'small'
+})
+
+Vue.config.productionTip = false
 
 new Vue({
-  render: (h) => h(App),
-}).$mount("#app");
+  el: '#app',
+  router,
+  store,
+  render: (h) => h(App)
+})
